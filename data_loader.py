72101b6571402dae862b56860b1475d6215563ce
April <aprilyoung010@gmail.com>
2018-09-17 19:55:36 +0800

    feat:build a encoder loader

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,7 @@ import torch
 from PIL import Image
 import numpy as np
 import pandas as pd
+import time
 import random
 import os
 import sys
@@ -30,7 +31,7 @@ def get_loader(transform, class_size=61, mode='train', batch_size=1, data_path='
     return data_loader
 
 
-def get_encoder_loader(transform, encoder, class_size=61, mode='train', batch_size=1, data_path='data'):
+def get_encoder_loader(transform, encoder, device, class_size=61, mode='train', batch_size=1, data_path='data', file=None):
     """Return a data loader that contain only embedding images"""
     
     assert mode in ['train', 'test'], "mode must be one of 'train' or 'test'"
@@ -41,7 +42,7 @@ def get_encoder_loader(transform, encoder, class_size=61, mode='train', batch_si
         img_folder = os.path.join(data_path, '2018_validationset_20180905/AgriculturalDisease_validationset/images/')
         notation_file = os.path.join(data_path, '2018_validationset_20180905/AgriculturalDisease_validationset/AgriculturalDisease_validation_annotations.json')
     
-    dataset = EncoderSet(transform, encoder, class_size, notation_file, img_folder, batch_size)
+    dataset = EncoderSet(transform, encoder, device, class_size, notation_file, img_folder, batch_size, file)
     
 
     data_loader = data.DataLoader(dataset=dataset,
@@ -52,27 +53,39 @@ def get_encoder_loader(transform, encoder, class_size=61, mode='train', batch_si
     
     
 class EncoderSet(data.Dataset):
-    def __init__(self, transforms, encoder, class_size, json_file, root_dir, batch_size):
+    def __init__(self, transforms, encoder, device, class_size, json_file, root_dir, batch_size, file=None):
         self.refer = pd.read_json(json_file)
         self.root_dir = root_dir
         self.transform = transforms
         self.encoder = encoder
         self.class_size = class_size
         self.batch_size = batch_size
-        self.embeddings = []
-        self.encode_data()
+        if file == None:
+            self.embeddings = []
+            self.encode_data(device)
+        else:
+            self.embedding = torch.load(file)
         
-    def encode_data(self):
+    def encode_data(self, device):
         total = len(self.refer)
-        for idx in range(len(self.refer)):
+        start_time = time.time()
+#         for idx in range(len(self.refer)):
+        for idx in range(32):
             image_path = os.path.join(self.root_dir, self.refer.iloc[idx, 1])
             image = Image.open(image_path).convert("RGB")
-            image = self.transform(image).unsqueeze(0).to(encoder.device)
+            image = self.transform(image).unsqueeze(0).to(device)
             embed = self.encoder(image)
             self.embeddings.append(embed)
-            print('\r'+"encoding {}/{}->{:.2f}%".format(idx, total, 100*idx/total), end='')
+            spent_time = time.time() - start_time
+            spent_time = "%d:%2.2f" % (spent_time//60, spent_time%60)
+            print('\r'+"encoding {}/{}->{:.2f}%, spent_time:{}".format(idx, total, 100*idx/total, spent_time), end='')
             sys.stdout.flush()
+            
+    def save_to(self, file):
+        """Save the encoded tensor to a specific file"""
+        torch.save(self.embeddings, file)
         
+    
     def __len__(self):
         return len(self.refer)
     
